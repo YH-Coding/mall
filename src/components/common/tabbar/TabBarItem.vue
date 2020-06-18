3603e068d74595cb256263b2d01555662e44c4ca
yinhui <358782295@qq.com>
2020-06-18 17:06:47 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+<div @click="itemClick" class="tab-bar-item">
+  <div v-if='!isActive'><slot name='item-icon'></slot></div>
+  <div v-else><slot name='item-icon-active'></slot></div>
+  <div :style="activeStyle"><slot name='item-text'></slot></div>
+</div>
+</template>
+
+<script>
+export default {
+name: 'TabBarItem',
+components: {},
+props: {
+    path: String,
+    activeColor: {
+        type: String,
+        default: 'red'
+    }
+},
+data() {
+return {
+    // isActivity: true
+};
+},
+computed: {
+    isActive(){
+        return this.$route.path.indexOf(this.path) != -1
+    },
+    activeStyle(){
+        return this.isActive ? {color:this.activeColor} : {}
+    }
+},
+methods: {
+    itemClick(){
+        this.$router.replace(this.path)
+    }
+},
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+    flex: 1;
+    /* height: 49px; */
+    padding-top: 3px;
+    text-align: center;
+    font-size: 14px;
+}
+.tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+    /* margin-bottom: 2px; */
+}
+.active {
+    color: red;
+}
+</style>
---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>购物车</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: 'Cart',
+components: {},
+data() {
+return {
+
+};
+},
+methods: {
+
+},
+}
+</script>
+
+<style scoped>
+</style>
