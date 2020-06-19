3603dffb910696e11c0c41ed2aa4cf53db9be272
yinhui <358782295@qq.com>
2020-06-19 09:42:19 +0800

    首页网络请求封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,17 +5,29 @@
 </template>
 
 <script>
+import {getHomeMultidata} from 'network/home'
 export default {
 name: 'Home',
 components: {},
 data() {
 return {
-
+    banners: [],
+    recommends: []
 };
 },
 methods: {
 
 },
+created() {
+    console.log('home created')
+    getHomeMultidata().then(res=>{
+        console.log(res)
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+        console.log(this.banners)
+        console.log(this.recommends)
+    })
+}
 }
 </script>
 
