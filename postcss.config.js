ccce06f2d66955f49b50681bfc062597d3555e65
yinhui <358782295@qq.com>
2020-07-27 17:03:28 +0800

    使用postcss-to-viewport完成项目中ppx自动转化vw

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px',
+      viewportWidth: 375,
+      unitPrecision: 5,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: ['tab-bar'],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: undefined,
+      include: undefined,
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 568
+    }
+  }
+}
